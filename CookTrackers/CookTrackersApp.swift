185931f9822b7286beec1e3550ceea5026f6f0b1
Tsubasa Kubota <tsubasa.kubota@example.com>
2025-06-14 20:40:31 +0900

    Initial Commit

---
+++ CookTrackers/CookTrackersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookTrackersApp.swift
+//  CookTrackers
+//
+//  Created by Tsubasa Kubota on 2025/06/14.
+//
+
+import SwiftUI
+
+@main
+struct CookTrackersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookTrackersTests/CookTrackersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookTrackersTests.swift
+//  CookTrackersTests
+//
+//  Created by Tsubasa Kubota on 2025/06/14.
+//
+
+import Testing
+@testable import CookTrackers
+
+struct CookTrackersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
